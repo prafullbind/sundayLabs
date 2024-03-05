10cc4462836d980147e52cc8258293ba12502d0b
Prafull Bind <prafullbind@gmail.com>
2024-03-05 17:56:36 +0530

    sql file added

---
+++ sundaylabsbackend/sundaylabs.sql
@@ -0,0 +1,12 @@
+
+-- Table structure for table `address_attributes`
+--Database name: sundaylabs
+
+CREATE TABLE cropImage (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    qualitySeeds VARCHAR(200),
+    soilManage VARCHAR(255),
+    irrigationManage VARCHAR(255),
+    diseaseControl VARCHAR(255),
+    imageData TEXT
+);
